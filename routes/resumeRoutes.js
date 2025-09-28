6c88ae18aa84de2a3be5626e91cd69ebb13cfb3a
Asad Amin Data Analyst | PowerBI Developer <asadamin236@gmail.com>
2025-09-28 17:09:16 +0500

    Updated Code

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
 import { createResume, getUserResumes, getResumeById, updateResume, deleteResume, duplicateResume } from "../controllers/resumeController.js";
+import { uploadImages } from "../controllers/uploadImages.js";
 const resumeRouter = express.Router();
 
 
@@ -9,6 +10,7 @@ resumeRouter.get("/", protect, getUserResumes);
 resumeRouter.get("/:id", protect, getResumeById);
 resumeRouter.put("/:id", protect, updateResume);
 resumeRouter.delete("/:id", protect, deleteResume);
+resumeRouter.put('/:id/upload-images', protect, uploadImages)
 resumeRouter.post("/:id/duplicate", protect, duplicateResume);
 
 export default resumeRouter;
